8c0aa8cc94cd42d450f426cbd0248b1e6483caed
VenkeyBoda <venkeyboda07@gmail.com>
2026-03-30 15:43:22 +0530

    updated backend and Dockerfile and src

--- Infra/terraform.tf
+++ Infra/terraform.tf
@@ -7,17 +7,17 @@ terraform {
     }
   }
   
-  # backend "azurerm" {
-  #   # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-  #   resource_group_name = "tfstate-rg"
-  #   # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-  #   storage_account_name = "venkat07"
-  #   # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-  #   container_name = "workshop-tfstate"
-  #   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  #   key = "workshop-tfstate"
+  backend "azurerm" {
+    # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    resource_group_name = "tfstate-rg"
+    # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    storage_account_name = "venkat07"
+    # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    container_name = "workshop-tfstate"
+    # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key = "workshop-tfstate"
 
-  # }
+  }
 
   required_version = ">= 1.10.0" 
 }
